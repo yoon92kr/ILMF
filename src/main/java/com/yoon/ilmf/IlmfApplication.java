a0eb7d8ad1075f17bba09fb07821ff9a02a19454
yoon92kr <yoon92_kr@naver.com>
2024-12-13 14:52:53 +0900

    init proeject

---
+++ src/main/java/com/yoon/ilmf/IlmfApplication.java
@@ -0,0 +1,13 @@
+package com.yoon.ilmf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlmfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlmfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yoon/ilmf/IlmfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoon.ilmf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlmfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
